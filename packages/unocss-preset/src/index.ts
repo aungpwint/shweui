98b078c50526f53a447517f63fac5bed1878d1f9
Aung Pwint <aungpwintofficial@gmail.com>
2025-02-08 02:10:26 +0630

    config: unocss-preset

--- packages/unocss-preset/src/index.ts
+++ packages/unocss-preset/src/index.ts
@@ -1,15 +1,4 @@
-import {
-    definePreset,
-    presetUno,
-    presetWind,
-    presetTagify,
-    presetAttributify,
-    presetIcons,
-    presetWebFonts,
-    transformerDirectives,
-    transformerVariantGroup,
-    transformerCompileClass
-} from 'unocss'
+import { definePreset } from 'unocss'
 import type { Theme } from '@unocss/preset-mini'
 import { random } from 'magic-color'
 import { shortcuts } from './shortcuts'
@@ -20,32 +9,17 @@ import type { PrsetShweUiOptions, ResolveShweUiOptions } from './types'
 import { preflights } from './preflights'
 import { LAYER_SHWE_UI_PRESET, LAYER_SHWE_UI } from './layers'
 
-function resolveOptions(options: PrsetShweUiOptions = {}): ResolveShweUiOptions {
-    const defaultOptions: Required<PrsetShweUiOptions> = {
-        color: 'auto',
-        preflights: true,
-        fonts: [], // 'DM Sans', 'DM Sans:400,700'
-        icons: {}
-    }
-
-    const resolvedOptions: ResolveShweUiOptions = {
-        ...defaultOptions,
-        ...options,
-        color: options.color === 'auto' ? random() : options.color!,
-        icons: {
-            ...defaultOptions.icons,
-            ...(options.icons ?? {})
-        }
-    }
-
-    return resolvedOptions
-}
+const resolveOptions = (options: PrsetShweUiOptions = {}): ResolveShweUiOptions => ({
+    color: options.color === 'auto' ? random() : options.color ?? 'auto',
+    preflights: options.preflights ?? true,
+    fonts: options.fonts ?? [],
+    icons: { ...options.icons }
+})
 
 export type { PrsetShweUiOptions }
 
 export const presetShweUi = definePreset<PrsetShweUiOptions, Theme>((options) => {
     const resolvedOptions = resolveOptions(options)
-
     return {
         name: '@shweui/unocss-preset',
         layer: LAYER_SHWE_UI_PRESET,
@@ -54,41 +28,9 @@ export const presetShweUi = definePreset<PrsetShweUiOptions, Theme>((options) =>
         shortcuts,
         variants,
         preflights: preflights(resolvedOptions),
-        presets: [
-            presetUno({ preflight: resolvedOptions.preflights, dark: 'class', attributify: false }),
-            presetWind(), // { important: '#app'}
-            presetAttributify(),
-            presetTagify({
-                prefix: 'un-'
-            }),
-            presetIcons({}),
-            presetWebFonts({
-                fonts: {
-                    onu: resolvedOptions.fonts
-                },
-                inlineImports: false
-            })
-        ],
-        transformers: [
-            transformerDirectives({
-                applyVariable: ['--at-apply', '--uno-apply', '--uno']
-            }),
-            transformerVariantGroup(),
-            transformerCompileClass({
-                classPrefix: 'uno-'
-            })
-        ],
-        extendTheme(defaultTheme) {
-            const { colors } = theme(resolvedOptions)
-
-            return {
-                ...defaultTheme,
-                colors: {
-                    ...defaultTheme.colors,
-                    ...colors
-                }
-            }
-        },
-        options: resolvedOptions
+        extendTheme: (defaultTheme) => ({
+            ...defaultTheme,
+            colors: { ...defaultTheme.colors, ...theme(resolvedOptions).colors }
+        })
     }
 })
