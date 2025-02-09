1f4d1ebd1e0e29b6f5337713f310391d80795d35
Aung Pwint <aungpwintofficial@gmail.com>
2025-02-09 22:14:38 +0630

    Primitive

--- packages/components/src/Primitive/Primitive.ts
+++ packages/components/src/Primitive/Primitive.ts
@@ -32,3 +32,30 @@ export interface PrimitiveProps {
      */
     as?: AsTag | Component
 }
+
+export const Primitive = defineComponent({
+    name: 'Primitive',
+    inheritAttrs: false,
+    props: {
+        asChild: {
+            type: Boolean,
+            default: false
+        },
+        as: {
+            type: [String, Object] as PropType<AsTag | Component>,
+            default: 'div'
+        }
+    },
+    setup(props, { attrs, slots }) {
+        const asTag = props.asChild ? 'template' : props.as
+
+        // For self closing tags, don't provide default slots because of hydration issue
+        const SELF_CLOSING_TAGS = ['area', 'img', 'input']
+        if (typeof asTag === 'string' && SELF_CLOSING_TAGS.includes(asTag))
+            return () => h(asTag, attrs)
+
+        if (asTag !== 'template') return () => h(props.as, attrs, { default: slots.default })
+
+        return () => h(Slot, attrs, { default: slots.default })
+    }
+})
